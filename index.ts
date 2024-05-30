e8c8b36279ccbe5b20e1e2d3f9ce6e10bab4916a
Piotr Karczewski <p.karczewsk@gmail.com>
2024-05-30 19:07:08 +0100

    Initial repo setup with simple bun server

---
+++ index.ts
@@ -0,0 +1,21 @@
+const server = Bun.serve({
+  async fetch(req) {
+    const path = new URL(req.url).pathname;
+
+    if (path === "/") {
+      return new Response(Bun.file("./index.html"), {
+        headers: { "content-type": "text/html" },
+      });
+    }
+
+    if (path === "/index.js") {
+      return new Response(Bun.file("./src/index.ts"), {
+        headers: { "content-type": "application/javascript" },
+      });
+    }
+
+    return Response.redirect("/", 301);
+  },
+});
+
+console.log(`Listening on ${server.url}`);
