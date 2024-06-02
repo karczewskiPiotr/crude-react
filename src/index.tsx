435554e49e20a89bcca35e36db6e131108c22e29
Piotr Karczewski <p.karczewsk@gmail.com>
2024-06-02 15:47:36 +0100

    feat: expand the demo

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,24 @@ import * as React from "./lib/jsx-dev-runtime";
 
 function Counter() {
   const [state, setState] = React.useState(1);
-  return <h1 onClick={() => setState((count) => count + 1)}>Count: {state}</h1>;
+
+  return (
+    <div>
+      <h1>Count: {state}</h1>
+      <button onClick={() => setState((count) => count + 1)}>Increment</button>
+      <button onClick={() => setState((count) => count - 1)}>Decrement</button>
+      <ol>
+        {Array.from({ length: 10 }).map((_, i) => (
+          <li>{i + 1 + state}</li>
+        ))}
+      </ol>
+      {state % 2 === 0 ? (
+        <h2 id="even">Count is even</h2>
+      ) : (
+        <h2 id="odd">Count is odd</h2>
+      )}
+    </div>
+  );
 }
 
 const element = <Counter />;
